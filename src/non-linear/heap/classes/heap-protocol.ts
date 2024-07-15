ee80501fc1ea0c8a335b9a1f1185ef8aee376cad
Artur Donda <artur.donda@outlook.com>
2024-07-15 07:59:55 -0300

    Refactor Heap protocol to handle fixUpward and fixDownward logic

--- src/non-linear/heap/classes/heap-protocol.ts
+++ src/non-linear/heap/classes/heap-protocol.ts
@@ -2,12 +2,8 @@ export abstract class HeapProtocol<T> {
 	protected heap: T[];
 	protected lastIndex: number;
 
-	/*
-	Sorts the heap to make sure the heap property is still valid.
-	It is abstract because it's implementation varies from a Max heap and a Min heap.
-	*/
-	protected abstract fixUpward(): void; // Fixes the heap property from bottom to top
-	protected abstract fixDownward(): void; // Fixes the heap property from top to bottom
+	// Checks if the heap property is valid for given two elements
+	protected abstract isHeapPropertyValid(firstIndex: number, secondIndex: number): boolean;
 
 	constructor(protected readonly getPriorityFunction: (data: T) => number) {
 		this.heap = [];
@@ -29,12 +25,13 @@ export abstract class HeapProtocol<T> {
 	}
 
 	insert(data: T): HeapProtocol<T> {
-		// Adds new item to the last position
+		// Increments lastIndex
 		this.lastIndex++;
 
+		// Adds new item to the last position
 		this.heap[this.lastIndex] = data;
 
-		// Then calls abstract fixUpward method to sort and apply heap property
+		// Calls fixUpward method to sort and apply heap property from bottom to top (as the new element was inserted at the bottom)
 		this.fixUpward();
 
 		return this;
@@ -43,34 +40,72 @@ export abstract class HeapProtocol<T> {
 	get(): T | null {
 		if (this.isEmpty()) return null;
 
+		// Get root
 		const root = this.heap[0];
-		const lastItem = this.heap[this.lastIndex];
 
-		this.heap[0] = lastItem;
+		// Swap root and last item
+		this.swapPositions(0, this.lastIndex);
 
+		// Remove last item (root) from heap
 		this.lastIndex--;
 		this.heap.length = this.lastIndex + 1;
 
+		// Calls fixDownward method to sort and apply heap property from top to bottom (as the last element is now position at the root)
 		this.fixDownward();
 
 		return root;
 	}
 
+	// Fixes the heap property from bottom to top - used on insert method
+	protected fixUpward(): void {
+		let currentIndex = this.lastIndex;
+		let parentIndex = this.getParentIndex(currentIndex);
+
+		while (parentIndex >= 0 && !this.isHeapPropertyValid(parentIndex, currentIndex)) {
+			this.swapPositions(parentIndex, currentIndex);
+
+			currentIndex = parentIndex;
+			parentIndex = this.getParentIndex(currentIndex);
+		}
+	}
+
+	// Fixes the heap property from top to bottom - used on delete method
+	protected fixDownward(): void {
+		if (this.isEmpty()) return;
+
+		let currentIndex = 0;
+
+		while (currentIndex <= this.lastIndex) {
+			let leftChildIndex = this.getLeftChildIndex(currentIndex);
+			let rightChildIndex = this.getRightChildIndex(currentIndex);
+
+			// as rightChildIndex is bigger than leftChildIndex, if leftChildIndex is out of bound, then right child is out of bound as well
+			if (leftChildIndex > this.lastIndex) break;
+
+			const childToSwap =
+				// if only right child is out of bound
+				rightChildIndex > this.lastIndex
+					? leftChildIndex // then use left child
+					: // otherwise, if both children are in range, use the one with highest priority
+					this.isHeapPropertyValid(leftChildIndex, rightChildIndex)
+					? leftChildIndex
+					: rightChildIndex;
+
+			if (this.isHeapPropertyValid(currentIndex, childToSwap)) break;
+
+			this.swapPositions(currentIndex, childToSwap);
+			currentIndex = childToSwap;
+		}
+	}
+
+	//#region Helpers
+	// Swaps two elements
 	protected swapPositions(firstIndex: number, secondIndex: number) {
 		const temp: T = this.heap[firstIndex];
 
 		this.heap[firstIndex] = this.heap[secondIndex];
 		this.heap[secondIndex] = temp;
 	}
-
-	//#region Get relative nodes
-
-	/*
-	For a given heap node at index i:
-		left child index = 2 * i + 1
-		right child index = 2 * i + 2
-		parent index = (i - 1) / 2
-	*/
 
 	protected getLeftChildIndex(index: number) {
 		return 2 * index + 1;
@@ -83,7 +118,7 @@ export abstract class HeapProtocol<T> {
 	protected getParentIndex(index: number) {
 		return Math.floor((index - 1) / 2);
 	}
-	//#endregion Get relative nodes
+	//#endregion Helpers
 
 	//#region Print
 	[Symbol.for('nodejs.util.inspect.custom')]() {
--- src/non-linear/heap/classes/max-heap.ts
+++ src/non-linear/heap/classes/max-heap.ts
@@ -1,42 +1,7 @@
 import { HeapProtocol } from './heap-protocol';
 
 export class MaxHeap<T> extends HeapProtocol<T> {
-	protected fixUpward(): void {
-		let currentIndex = this.lastIndex;
-		let parentIndex = this.getParentIndex(currentIndex);
-
-		while (parentIndex >= 0 && this.getPriorityFunction(this.heap[currentIndex]) > this.getPriorityFunction(this.heap[parentIndex])) {
-			this.swapPositions(currentIndex, parentIndex);
-
-			currentIndex = parentIndex;
-			parentIndex = this.getParentIndex(currentIndex);
-		}
-	}
-
-	protected fixDownward(): void {
-		if (this.isEmpty()) return;
-
-		let currentIndex = 0;
-
-		while (currentIndex <= this.lastIndex) {
-			let leftChildIndex = this.getLeftChildIndex(currentIndex);
-			let rightChildIndex = this.getRightChildIndex(currentIndex);
-
-			// as rightChildIndex is bigger than leftChildIndex, if leftChildIndex is out of bound, then right child is out of bound as well
-			if (leftChildIndex > this.lastIndex) break;
-
-			const childToSwap =
-				rightChildIndex > this.lastIndex
-					? leftChildIndex // if only right child is out of bound, use left child
-					: // otherwise if both children are in range, use the one with biggest priority
-					this.getPriorityFunction(this.heap[leftChildIndex]) > this.getPriorityFunction(this.heap[rightChildIndex])
-					? leftChildIndex
-					: rightChildIndex;
-
-			if (this.getPriorityFunction(this.heap[currentIndex]) > this.getPriorityFunction(this.heap[childToSwap])) break;
-
-			this.swapPositions(currentIndex, childToSwap);
-			currentIndex = childToSwap;
-		}
+	protected isHeapPropertyValid(firstIndex: number, secondIndex: number): boolean {
+		return this.getPriorityFunction(this.heap[firstIndex]) > this.getPriorityFunction(this.heap[secondIndex]);
 	}
 }
